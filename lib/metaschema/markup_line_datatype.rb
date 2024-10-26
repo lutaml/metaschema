9b5d8f072b782024a4bccfcee667621cc92a9f00
Ronald Tse <ronald.tse@ribose.com>
2024-10-26 19:31:22 +0800

    feat: use mixed content to represent text

--- lib/metaschema/markup_line_datatype.rb
+++ lib/metaschema/markup_line_datatype.rb
@@ -23,7 +23,7 @@ module Metaschema
     attribute :img, ImageType, collection: true
 
     xml do
-      root "MarkupLineDatatype", ordered: true
+      root "MarkupLineDatatype", mixed: true
       namespace "http://csrc.nist.gov/ns/oscal/metaschema/1.0"
 
       map_content to: :content
